6639acd6b838fbd08d0e0af2e85a0c50a2935aa3
Menium <97258926+menium878@users.noreply.github.com>
2024-12-09 16:37:22 +0100

    Update Day_9.cc
    Just input will put the rest after

--- Day_9.cc
+++ Day_9.cc
@@ -1,1 +1,32 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
 
+
+int main() {
+    
+
+std::ifstream file("cos.txt"); 
+char ch;
+    std::string line;
+while(std::getline(file,line)){
+}
+//std::cout<<line;
+std::string first;
+int count=0;
+for (size_t i = 0; i < line.size(); i++)
+{
+    if(i%2==0){
+        for(size_t k =0;k<(line[i]- '0');k++)
+        first+=(std::to_string(count));
+        count++;
+        continue;
+    }
+    for(size_t k =0;k<(line[i]- '0');k++)
+    first+=".";
+}
+file.close();
+return 0;
+}
